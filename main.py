4fa0ce1db35655de10ca162488e75d4713c26fc0
Preston Jenkins <preston.b.jenkins-1@ou.edu>
2023-04-18 13:22:33 -0500

    🛠️refactor(main) refactor receive_prompt method

--- main.py
+++ main.py
@@ -12,61 +12,57 @@ midjourney_bot = MidjourneyBot()
 
 app = Flask(__name__)
 
+INVALID_PROMPT_ERROR = {"status": "error", "message": "Invalid prompt."}
+
+
+def wait_for_image():
+    while True:
+        message = midjourney_bot.messages(1)[0]
+        if midjourney_bot.validate_image_url(message):
+            break
+        print(midjourney_bot.content(message))
+        midjourney_bot.wait()
+
 
 @app.route("/prompt", methods=["POST"])
 def receive_prompt():
     data = request.get_json()
     prompt = data.get("prompt", None)
     refId = data.get("refId", None)
-    time.sleep(2)
-    if prompt:
-        time.sleep(2)
-        midjourney_bot.ask(prompt)
-        time.sleep(2)
 
-        headers = {}
-        additional_data = {
-            "refId": f"{refId}",  # Replace this with the actual refId
-            "ref": "api::article.article",
-            "field": "image",
-        }
-        external_url = WEBHOOK_URL
+    if not prompt:
+        return jsonify(INVALID_PROMPT_ERROR)
 
-        # Wait for the original image to be generated
-        while True:
-            message = midjourney_bot.messages(1)[0]
-            if midjourney_bot.validate_image_url(message):
-                break
-            print(midjourney_bot.content(message))
-            time.sleep(5)
+    midjourney_bot.wait()
+    midjourney_bot.ask(prompt)
+    midjourney_bot.wait()
 
-        # Trigger the up_scale command
-        original_message = midjourney_bot.messages(1)[0]
-        print(original_message)
-        up_scale_status = midjourney_bot.up_scale(1, original_message)
-        print("Up_scale status:", up_scale_status)
+    headers = {}
+    additional_data = {
+        "refId": f"{refId}",
+        "ref": "api::article.article",
+        "field": "image",
+    }
 
-        # Wait for the upscaled image to be generated
-        while True:
-            upscaled_message = midjourney_bot.messages(1)[0]
-            if (
-                midjourney_bot.validate_image_url(upscaled_message)
-                and upscaled_message["id"] != original_message["id"]
-            ):
-                break
-            print(midjourney_bot.content(upscaled_message))
-            time.sleep(5)
+    # Wait for the original image to be generated
+    wait_for_image()
 
-        upscaled_message = midjourney_bot.messages(1)[0]
-        image_url = midjourney_bot.get_image_url(upscaled_message)
+    # Trigger the up_scale command
+    original_message = midjourney_bot.messages(1)[0]
+    print(original_message)
+    up_scale_status = midjourney_bot.up_scale(1, original_message)
+    print("Up_scale status:", up_scale_status)
 
-        midjourney_bot.save_image(image_url, external_url, headers, additional_data)
+    # Wait for the upscaled image to be generated
+    wait_for_image()
 
-        return jsonify(
-            {"status": "success", "message": "Prompt received and processed."}
-        )
-    else:
-        return jsonify({"status": "error", "message": "Invalid prompt."})
+    upscaled_message = midjourney_bot.messages(1)[0]
+    image_url = midjourney_bot.get_image_url(upscaled_message)
+
+    payload = midjourney_bot.save_image(
+        image_url, WEBHOOK_URL, headers, additional_data
+    )
+    return jsonify(payload)
 
 
 if __name__ == "__main__":
